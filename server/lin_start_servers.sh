cbfa4f96274c14694b7ec8e5998bf0716ee332e1
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-12-31 13:48:36 +0100

    Simplify network

--- server/lin_start_servers.sh
+++ server/lin_start_servers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 0 1 12)
+for i in $(seq 0 1 7)
 do
   gnome-terminal -e go run . i
 done
--- server/win_start_servers.bat
+++ server/win_start_servers.bat
@@ -1,3 +1,3 @@
-for /l %%x in (0, 1, 12) do (
+for /l %%x in (0, 1, 7) do (
    start cmd /k go run . %%x
 )
